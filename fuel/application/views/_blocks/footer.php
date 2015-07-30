a7372393be2c225c83f7f23c3d0ed192cf5fb49d
Marjaana Viitanen <marjaana.viitanen@student.tut.fi>
2015-07-30 21:39:18 +0300

    Lisätty simppeli ulkoasu

--- fuel/application/views/_blocks/footer.php
+++ fuel/application/views/_blocks/footer.php
@@ -1,4 +1,18 @@
-<p>Tämä on footer</p>
+</div> 
+  	</div>
+</div>
 
-</body>
+
+
+	<!-- script references -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+	<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+		<script>
+		
+		$(document).ready(function(){
+
+		});
+		</script>
+	</body>
 </html>
--- fuel/application/views/_blocks/header.php
+++ fuel/application/views/_blocks/header.php
@@ -1,17 +1,77 @@
 <!DOCTYPE html>
-<html lang="fi">
-<head>
-	<meta charset="UTF-8">
-	<?php echo css('vrl.css'); ?>
- 	<title>
+<html lang="en">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>
 		<?php 
 			echo fuel_var('page_title', '');
 		?>
-	</title>
+		</title>
+		<meta name="generator" content="Bootply" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<style type="text/css">
+		<!--
+			header {
+				margin-bottom:30px;
+			}
 
-</head>
-<body>
+		-->
+		</style>
+		<?php echo css('vrl.css'); ?>
+	</head>
+	<body>
 
-<p>Tämä on header</p>
+<header class="navbar navbar-default navbar-static-top" role="banner">
+  <div class="container">
+    <div class="navbar-header">
+      <button class="navbar-toggle" type="button" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a href="/" class="navbar-brand">VRL</a>
+    </div>
+    <nav class="collapse navbar-collapse" role="navigation">
+      <ul class="nav navbar-nav">
+        <li>
+          <a href="#">Get Started</a>
+        </li>
+        <li>
+          <a href="#">Edit</a>
+        </li>
+        <li>
+          <a href="#">Visualize</a>
+        </li>
+        <li>
+          <a href="#">Prototype</a>
+        </li>
+      </ul>
+    </nav>
+  </div>
+</header>
 
-<h1>VRL</h1>
+<!-- Begin Body -->
+<div class="container">
+	<div class="row">
+  			<div class="col-md-3" id="leftCol">
+              	
+				<div class="well"> 
+              	<ul class="nav nav-stacked" id="sidebar">
+                  <li><a href="#sec1">Section 1</a></li>
+                  <li><a href="#sec2">Section 2</a></li>
+                  <li><a href="#sec3">Section 3</a></li>
+                  <li><a href="#sec4">Section 4</a></li>
+              	</ul>
+  				</div>
+
+      		</div>  
+      		<div class="col-md-9">
+
+	
+<h2 id="sec0">VRL</h2>
