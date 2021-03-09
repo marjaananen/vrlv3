7dd2b7205aea1e4cd0f12236728ecef868fa1532
Marjaana Karttunen <marjaana.karttunen@atostek.com>
2021-03-09 19:44:17 +0200

    Tekstien päivitys

--- fuel/application/views/liitto/somessa.php
+++ fuel/application/views/liitto/somessa.php
@@ -1,5 +1,7 @@
 <h1>VRL sosiaalisessa mediassa</h1>
 
-<p>Virtuaalisen Ratsastajainliiton viralliset sosiaalisen median kanavat ovat Facebook (https://www.facebook.com/virtuaalihevoset/) ja Instagram (https://www.instagram.com/virtuaalihevoset/).</p>
+<p>Virtuaalisen Ratsastajainliiton viralliset sosiaalisen median kanavat ovat Facebook
+(<a href="https://www.facebook.com/virtuaalihevoset/">https://www.facebook.com/virtuaalihevoset/</a>) ja Instagram
+(<a href="https://www.instagram.com/virtuaalihevoset/">https://www.instagram.com/virtuaalihevoset/</a>).</p>
 
 <p>Sosiaalisen median kanavien tarkoituksena on lisätä tietoa virtuaalihevosista muun muassa erinäisin esittelyin, tapahtumatempauksin ja ajankohtaisista asioista informoiden. Tuotamme erityisesti Instagramiin monipuolista sisältöä, jonka sisältöön pääsee vaikuttamaan myös harrastajat. Jos sinulla on juttuvinkki esimerkiksi ajankohtaisesta tapahtumasta tai vaikka rodun tai lajin esittelystä, olethan yhteydessä sosiaalisen median tuottajiin joko sähköpostitse virtuaalinenratsastajainliitto@gmail.com tai Instagramin chatviestitoiminnolla. </p>
