9df54e9fb5b288464a7b8f5b530c9a95b04120bc
Marjaana Karttunen <marjaana.karttunen@atostek.com>
2022-01-30 13:13:47 +0200

    Lisätty puuttuvia sivuja.

---
+++ fuel/application/views/jalostus/perinnollisyys.php
@@ -0,0 +1,55 @@
+<h1>Perinnöllisyys virtuaalimaailmassa</h1>
+
+<div class="panel-group">
+    <div class="panel panel-default">
+      <div class="panel-heading">Nyrkkisääntö</div>
+      <div class="panel-body">
+        <p>Virtuaalimaailmassa mikään ei periydy automaattisesti, joten voit päättää vapaasti miten käytät hevosiasi jalostukseen. Monet kuitenkin pitävät realistisuudesta, ja siksi koetetaan välttää esimerkiksi liiallista sisäsiittoisuutta. VRL tarjoaa välineet sukusiitosprosenttien ja värien periytymisen yms. laskemiseen ja ominaisuuspisteiden periytymiseen rekisteröidyille hevosille, mutta näiden noudattaminen ei ole millään tavalla pakollista!</p>
+        <p>Mikäli et ole kiinnostunut numeroiden tuijottelusta etkä tahdo tehdä kasvatuksesta liian monimutkaista, mutta haluat silti jalostustoimintaasi enemmän realistisuutta, voit myös käyttää helppoja nyrkkisääntöjä: astuta tammat aina mahdollisimman erisukuisilla oreilla, ja koeta välttää samojen nimien kertautuminen esimerkiksi kolmessa ensimmäisessä polvessa, valitse varsan väriksi jokin vanhemmilla esiintyvä väri ja rekisteröi kaikki hevosesi. Mikään ei kuitenkaan estä poikkeamasta näistä säännöistä!
+        </p>
+      </div>
+    </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">Värien periytyminen</div>
+      <div class="panel-body">
+
+        <p>Virtuaalihevosten värit eivät periydy automaattisesti, ja hevonen voi myös vaihtaa väriä elämänsä aikana.
+        Värien periytyminen onkin kiinni virtuaalihevosen omistajan viitseliäisyydestä, eikä siksi kovin yleistä.
+        Harrastajat ovat kuitenkin enenevässä määrin kiinnostuneita värien periytymisestä.
+        </p>
+        
+        <p>Värien periytyminen alkaa siitä, että olemassa olevien hevosten värit osaa tunnistaa oikein.
+        Mikäli värien periytyminen ei ole tuttua, turvallisin vaihtoehto on valita varsalle sama väri kuin jommalla kummalla vanhemmalla on.
+        Tämä sääntö pätee lähes kaikkien yleisimpien värien kohdalla lukuun ottamatta tuplavoikkoja. Voit varmistaa valintasi VRL:n perinnöllisyyslaskurilla.</p>
+        
+        </div>
+     </div>
+    <div class="panel panel-default">
+      <div class="panel-heading">Sukusiitosprosentti</div>
+      <div class="panel-body">
+        <p>Sukusiitosprosentti eli sukusiitosaste kuvastaa sitä, miten lievästi tai voimakkaasti hevonen on suku- tai linjasiitetty. Paras mahdollinen sukusiitosprosentti on 0%. Kannattaa kuitenkin huomata, että isä ja emä voivat olla molemmat hyvinkin raskaasti sukusiitettyjä, ja varsan prosentti silti nolla! Se tarkoittaa vain, ettei isän ja emän puolella ole yhtään yhteisiä nimiä. Vaikka hevonen A esiintyisi emän puolella kymmeniä kertoja, ei se vaikuta varsan sukusiitosasteeseen, mikäli se ei esiinny isän puolella kertaakaan. Muistettava on kuitenkin, ettei sukusiitos ole yksinomaan hyvä tai huono asia: jokaisen rodun synty ja kehitys on tapahtunut tietynasteisella sukusiitoksella.
+        </p><p>
+        Sukusiitosprosentin suositeltu yläraja vaihtelee riippuen rodusta: jos valikoimaa on paljon, ei kasvattajien ole pakko hyväksyä korkeampia lukemia. Lisääntyvän sukulaisuuden vuoksi kaikkien suljettujen kantojen keskimääräinen sukusiitosaste nousee kuitenkin pikkuhiljaa. Esimerkiksi kaikki oikeat gotlanninrussit polveutuvat muutamasta kymmenestä ponista, joten vuosina 1999-2009 ruotsissa syntyneillä russeilla keskimääräinen sukusiittoisuuskerroin oli 10,4%, ja sukusiittoisuuden kasvu tänä aikana on ollut n. 0,5% per vuosi. Kaikkien vuonna 2014 suomessa hyväksyttyjen jalostusorien sukusiitosprosentti taas oli reilusti yli tuon keskimäärän. Tutkimuksissa on todettu sukusiitoksen haittavaikutusten alkavan näkyä vähän kerrallaan sukusiitosasteen ylittäessä 10%.
+        </p><p>
+
+        Sukusiitosasteen myötä- tai haittavaikutukset eivät kuitenkaan virtuaalihevosissa näy, koska niiden kanta ei ole suljettu (uusia hevosia voidaan keksiä jatkuvasti lisää), ei sukusiitoksen harrastaminen suurissa määrin ole tarpeellista. Ilman hyvää syytä ja suunnitelmaa ei siis kannata teettää yhdistelmiä, joiden sukusiitosprosentti kipuaa yli 10%. Järkevimpiä yhdistelmiä ovat sellaiset, että jälkeläisille tulee prosentiksi 0% tai ainakin pienempi prosentti kuin vanhemmilleen.
+        </p>
+                <p>Sukusiitosprosentti näkyy hevosen rekisterisivulla ja Unelmasuku-hakuohjelmassa. Prosentti on kuitenkin laskettu ainoastaan rekisteröityjen sukulaisten perusteella!</p>
+
+</div>
+    </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">Sukukatokerroin</div>
+      <div class="panel-body">
+        <p>Sukukatokerroin kuvastaa sitä, kuinka paljon suvussa kertautuu samoja nimiä koko suvun nimimäärään verrattuna. Siinä missä sukusiitosprosentista halutaan mahdollisimman pieni, sukukatokertoimesta halutaan mahdollisimman suuri. Sukusiitosprosentti ei huomioi isän ja emän omaa sukusiittoisuutta, mikäli ne eivät ole sukua toisilleen, mutta sukukatokertoimessa nämäkin tulevat huomatuksi. Sukukatokerroin saadaan laskemalla kuinka monta eri ponia suvussa on, ja jakamalla se sillä määrällä, montako eri ponia suvussa voisi olla, jos ei esiintyisi yhtäkään samaa nimeä. Sukukatokertoin voi olla väliltä 0-1, joista 1 on paras arvo. Mitä pienempi luku on kyseessä, sen enemmän ponin suvussa kertautuvat samat nimet. Jos kerroin on 1, se tarkoittaa ettei sen suvussa kertaudu yksikään nimi. Jalostuksessa kannattaa pitää sukukatokerroin mahdollisimman suurena (ainakin yli 0.8, mieluiten enemmän) etenkin viiden sukupolven sisällä.
+
+        </p>
+        <p>Sukukatokerroin näkyy hevosen rekisterisivulla ja Unelmasuku-hakuohjelmassa. Kerroin on kuitenkin laskettu ainoastaan rekisteröityjen sukulaisten perusteella!</p>
+
+        
+        </div>
+    </div>
+
+
+    </div>
---
+++ fuel/application/views/jasenyys/liity.php
@@ -0,0 +1,9 @@
+<p>Virtuaalisen ratsastajainliiton jäsenyys, eli VRL-tunnus, on tarkoitettu virtuaalihevosista ja liiton toiminnasta kiinnostuneille henkilöillä iästä riippumatta. Jokainen jäsen saa yksilöllisen tunnuksen, joka toimii henkilötunnuksen tavoin virtuaalimaailmassa.</p>
+
+<p><b>Rekisteröitymällä jäseneksi voit mm.</b><br>
+-Lähettää pikaviestejä kavereiden kanssa, ja osallistua VRL:n päätöksentekoon.<br>
+-Rekisteröidä hevosia, kasvattajanimiä, talleja ja seuroja, ja muokata näiden tietoja reaaliaikaisesti.<br>
+- Osallistua kilpailuihin ja näyttelyihin, ja kantakirjata hevosia.</p>
+
+<p>Tunnukset ovat henkilökohtaisia<br>
+- Kukin henkilö on oikeutettu vain yhteen VRL-tunnukseen väärinkäytösten välttämiseksi! Jos olet unohtanut salasanasi tai tunnuksesi, älä rekisteröi uutta tunnusta, vaan palauta salasana tai ota yhteyttä ylläpitoon.</p>
